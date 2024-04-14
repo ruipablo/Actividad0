d8fb6ed5362ce7bea0e777aa00e29643bcd4491c
Pablo <pablodanielruival@gmail.com>
2024-04-14 11:36:19 -0300

    [Parte 4.3] Modificando vistas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,18 +5,26 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.db.models import F
+from django.views import generic
 
 
-def index(request):
-    return HttpResponse("Hola mundo,estas en el index de encuestas(pool)")
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
